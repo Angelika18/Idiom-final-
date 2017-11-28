1e1850c53616d45f78796cd6df50cf80da8408a2
Angelika18 <angelikaestrada06.ae@gmail.com>
2017-11-28 07:30:11 -0800

    idioms(final)

---
+++ idioms(final)/www/js/process.js
@@ -0,0 +1,301 @@
+var a1 = new Audio();
+a1.src = "audio/a1.mp3";
+function P1() {
+    a1.play();
+}
+
+var a2 = new Audio();
+a2.src = "audio/a2.mp3";
+function P2() {
+    a2.play();
+}
+
+var a3 = new Audio();
+a3.src = "audio/a3.mp3";
+function P3() {
+    a3.play();
+}
+
+var a4 = new Audio();
+a4.src = "audio/a4.mp3";
+function P4() {
+    a4.play();
+}
+
+var a5 = new Audio();
+a5.src = "audio/a5.mp3";
+function P5() {
+    a5.play();
+}
+
+var a6 = new Audio();
+a6.src = "audio/a6.mp3";
+function P6() {
+    a6.play();
+}
+
+var a7 = new Audio();
+a7.src = "audio/a7.mp3";
+function P7() {
+    a7.play();
+}
+
+var a8 = new Audio();
+a8.src = "audio/a8.mp3";
+function P8() {
+    a8.play();
+}
+
+var a9 = new Audio();
+a9.src = "audio/a9.mp3";
+function P9() {
+    a9.play();
+}
+
+var a10 = new Audio();
+a10.src = "audio/a10.mp3";
+function P10() {
+    a10.play();
+}
+
+
+var b1 = new Audio();
+b1.src = "audio/b1.mp3";
+function P11() {
+    b1.play();
+}
+
+var b2 = new Audio();
+b2.src = "audio/b2.mp3";
+function P12() {
+    b2.play();
+}
+
+
+var b3 = new Audio();
+b3.src = "audio/b3.mp3";
+function P13() {
+    b3.play();
+}
+
+var b4 = new Audio();
+b4.src = "audio/b4.mp3";
+function P14() {
+    b4.play();
+}
+
+var b5 = new Audio();
+b5.src = "audio/b5.mp3";
+function P15() {
+    b5.play();
+}
+
+var b6 = new Audio();
+b6.src = "audio/b6.mp3";
+function P16() {
+    b6.play();
+}
+
+var b7 = new Audio();
+b7.src = "audio/b7.mp3";
+function P17() {
+    b7.play();
+}
+
+var b8 = new Audio();
+b8.src = "audio/b8.mp3";
+function P18() {
+    b8.play();
+}
+
+var b9 = new Audio();
+b9.src = "audio/b9.mp3";
+function P19() {
+    b9.play();
+}
+
+var b10 = new Audio();
+b10.src = "audio/b10.mp3";
+function P20() {
+    b10.play();
+}
+
+var c1 = new Audio();
+c1.src = "audio/c1.mp3";
+function P21() {
+    c1.play();
+}
+
+var c2 = new Audio();
+c2.src = "audio/c2.mp3";
+function P22() {
+    c2.play();
+}
+
+var c3 = new Audio();
+c3.src = "audio/c3.mp3";
+function P23() {
+    c3.play();
+}
+
+var c4 = new Audio();
+c4.src = "audio/c4.mp3";
+function P24() {
+    c4.play();
+}
+
+var c5 = new Audio();
+c5.src = "audio/c5.mp3";
+function P25() {
+    c5.play();
+}
+
+var c6 = new Audio();
+c6.src = "audio/c6.mp3";
+function P26() {
+    c6.play();
+}
+
+var c7 = new Audio();
+c7.src = "audio/c7.mp3";
+function P27() {
+    c7.play();
+}
+
+var c8 = new Audio();
+c8.src = "audio/c8.mp3";
+function P28() {
+    c8.play();
+}
+
+var c9 = new Audio();
+c9.src = "audio/c9.mp3";
+function P29() {
+    c9.play();
+}
+
+var c10 = new Audio();
+c10.src = "audio/c10.mp3";
+function P30() {
+    c10.play();
+}
+
+var d1 = new Audio();
+d1.src = "audio/d1.mp3";
+function P31() {
+    d1.play();
+}
+
+var d2 = new Audio();
+d2.src = "audio/d2.mp3";
+function P32() {
+    d2.play();
+}
+
+var d3 = new Audio();
+d3.src = "audio/d3.mp3";
+function P33() {
+    d3.play();
+}
+
+var d4 = new Audio();
+d4.src = "audio/d4.mp3";
+function P34() {
+    d4.play();
+}
+
+var d5 = new Audio();
+d5.src = "audio/d5.mp3";
+function P35() {
+    d5.play();
+}
+
+var d6 = new Audio();
+d6.src = "audio/d6.mp3";
+function P36() {
+    d6.play();
+}
+
+var d7 = new Audio();
+d7.src = "audio/d7.mp3";
+function P37() {
+    d7.play();
+}
+
+var d8 = new Audio();
+d8.src = "audio/d8.mp3";
+function P38() {
+    d8.play();
+}
+
+var d9 = new Audio();
+d9.src = "audio/d9.mp3";
+function P39() {
+    d9.play();
+}
+
+var d10 = new Audio();
+d10.src = "audio/d10.mp3";
+function P40() {
+    d10.play();
+}
+
+var e1 = new Audio();
+e1.src = "audio/e1.mp3";
+function P41() {
+    e1.play();
+}
+
+var e2 = new Audio();
+e2.src = "audio/e2.mp3";
+function P42() {
+    e2.play();
+}
+
+var e3 = new Audio();
+e3.src = "audio/e3.mp3";
+function P43() {
+    e3.play();
+}
+
+var e4 = new Audio();
+e4.src = "audio/e4.mp3";
+function P44() {
+    e4.play();
+}
+
+var e5 = new Audio();
+e5.src = "audio/e5.mp3";
+function P45() {
+    e5.play();
+}
+
+var e6 = new Audio();
+e6.src = "audio/e6.mp3";
+function P46() {
+    e6.play();
+}
+
+var e7 = new Audio();
+e7.src = "audio/e7.mp3";
+function P47() {
+    e7.play();
+}
+
+var e8 = new Audio();
+e8.src = "audio/e8.mp3";
+function P48() {
+    e8.play();
+}
+
+var e9 = new Audio();
+e9.src = "audio/e9.mp3";
+function P49() {
+    e9.play();
+}
+
+var e10 = new Audio();
+e10.src = "audio/e10.mp3";
+function P50() {
+    e10.play();
+}
